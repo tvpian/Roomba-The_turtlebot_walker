2bea1529465d99636dda881113d45b4dff883370
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-12-06 02:04:54 -0500

    Modifying static analysis code

--- run_cppcheck.sh
+++ run_cppcheck.sh
@@ -1,2 +1,2 @@
-cppcheck --enable=all --std=c++17 --suppress=missingIncludeSystem $( find . -name *.cpp | grep -vE -e "/src/first_publisher/src/*.cpp" -e "/src/first_subscriber/src/*.cpp") > results/cppcheck.txt
+cppcheck --enable=all --std=c++11 -I include/ --suppress=missingInclude $( find . -name \*.hpp -or -name *.cpp | grep -vE -e "^./build/" -e "^./vendor/") >> results/cppcheck.txt
 
